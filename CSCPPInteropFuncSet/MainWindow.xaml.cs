a85b99af99dae92e8e46994f567c8a3c83c68f30
Calvin Hsia (from Dev Box) <calvinh@microsoft.com>
2024-10-23 15:59:02 -0700

    add cpp

--- CSCPPInteropFuncSet/MainWindow.xaml.cs
+++ CSCPPInteropFuncSet/MainWindow.xaml.cs
@@ -21,4 +21,14 @@ namespace CSCPPInteropFuncSet
             InitializeComponent();
         }
     }
+    public class DllHandler
+    { 
+    }
+    public interface ICPPInteropFuncSet
+    {
+        int Add(int a, int b);
+        int Subtract(int a, int b);
+        int Multiply(int a, int b);
+        int Divide(int a, int b);
+    }
 }
